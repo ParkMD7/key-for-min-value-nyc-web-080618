5a0c79277d8b2a3707a83238064146279b436962
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-26 03:05:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |name, value|
     if l_value == nil || value < l_value
-      l_value = l_key
+      l_value = value
       l_key = name
     end
   end
